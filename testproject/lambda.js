0d1d18044186ebf012520b77efa5137a090c9a11
HirudineeADRT <hirudinee@adroitlogic.com>
2020-03-06 17:00:31 +0530

    smoke test

---
+++ testproject/lambda.js
@@ -0,0 +1,17 @@
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = async (event) => {
+    // cognito_idp.listUsers({
+    //     UserPoolId: process.env.UserPoolId_cognitol,
+    //     Limit: 10
+    // }).promise()
+    //     .then(data => {
+    //         // your code goes here
+    //     })
+    //     .catch(err => {
+    //         // error handling goes here
+    //     });
+
+    return { "message": "Successfully executed" };
+};
